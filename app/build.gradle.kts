c0c6309e0afd15db4737bec1c62465ab4c66004f
artnoage <artnoage@gmail.com>
2024-08-13 17:29:08 +0200

    Final les

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.vaios.holobar"
         minSdk = 28
         targetSdk = 34
-        versionCode = 6
+        versionCode = 7
         versionName = "1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
